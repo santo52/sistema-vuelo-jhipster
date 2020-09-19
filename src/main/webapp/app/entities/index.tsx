edbc353c2693b2e9f7cf9f249cd5156d6d1d9593
Santiago Ruiz <santiago.ruiz@bewe.io>
2020-09-18 21:57:43 -0500

    remove avion

--- src/main/webapp/app/entities/index.tsx
+++ src/main/webapp/app/entities/index.tsx
@@ -4,7 +4,7 @@ import { Switch } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
-import Avion from './avion';
+// import Avion from './avion';
 import Aeropuerto from './aeropuerto';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
